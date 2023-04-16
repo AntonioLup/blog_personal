0673cacb0d16e127a70909abbd6548999d17ac27
oktio89 <wheeze20@gmail.com>
2023-04-16 23:33:12 +0200

    index

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,19 +1,24 @@
-import "./styles/Dropdown.scss"
+import "./styles/Dropdown.scss";
 interface DropdownMenuProps {
-    isOpen: boolean; // Especificar el tipo booleano para isOpen
-  }
-const DropdownMenu: React.FC<DropdownMenuProps> = ({isOpen}) => {
-    
-    return (
-      <div className="dropdown-menu">
-      
-        <ul className={`dropdown-menu__list ${isOpen ? 'open' : ''}`}>
+  isOpen: boolean; // Especificar el tipo booleano para isOpen
+}
+const DropdownMenu: React.FC<DropdownMenuProps> = ({ isOpen }) => {
+  return (
+    <div className="dropdown-menu">
+      <div>
+        <ul>
+          <li>article</li>
+        </ul>
+      </div>
+      <div>
+        <ul className={`dropdown-menu__list ${isOpen ? "open" : ""}`}>
           <li className="dropdown-menu__item">Opción 1</li>
           <li className="dropdown-menu__item">Opción 2</li>
           <li className="dropdown-menu__item">Opción 3</li>
         </ul>
       </div>
-    );
-  }
-  
-  export default DropdownMenu;
+    </div>
+  );
+};
+
+export default DropdownMenu;
